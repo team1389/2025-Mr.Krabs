b7a3bf013e8a9e54bab3612f3e63ec29fff8227e
Zach Wheeler <92531055+1352351253@users.noreply.github.com>
2024-12-05 18:11:07 -0500

    YAGSL Swerve Example Code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,61 +1,171 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
- * Don't change the name of this class since the VM is set up to run this
+ * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as
+ * described in the TimedRobot documentation. If you change the name of this class or the package after creating this
+ * project, you must also update the build.gradle file in the project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends TimedRobot
+{
 
-    /**
-     * Initialize all systems here as public & static.
-     * Ex: public static System system = new System();
-     */
+  private static Robot   instance;
+  private        Command m_autonomousCommand;
 
+  private OI m_OI;
 
-    public static OI oi = new OI();
+  private Timer disabledTimer;
 
+  public Robot()
+  {
+    instance = this;
+  }
 
-    @Override
-    public void robotInit() {
+  public static Robot getInstance()
+  {
+    return instance;
+  }
+
+  /**
+   * This function is run when the robot is first started up and should be used for any initialization code.
+   */
+  @Override
+  public void robotInit()
+  {
+    // Instantiate our OI.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_OI = new OI();
+
+    // Create a timer to disable motor brake a few seconds after disable.  This will let the robot stop
+    // immediately when disabled, but then also let it be pushed more 
+    disabledTimer = new Timer();
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics that you want ran
+   * during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic()
+  {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
+
+  /**
+   * This function is called once each time the robot enters Disabled mode.
+   */
+  @Override
+  public void disabledInit()
+  {
+    m_OI.setMotorBrake(true);
+    disabledTimer.reset();
+    disabledTimer.start();
+  }
+
+  @Override
+  public void disabledPeriodic()
+  {
+    if (disabledTimer.hasElapsed(RobotMap.DrivebaseConstants.WHEEL_LOCK_TIME))
+    {
+      m_OI.setMotorBrake(false);
+      disabledTimer.stop();
     }
+  }
 
-    /**
-     * This function is called every robot packet, no matter the mode. Use
-     * this for items like diagnostics that you want ran during disabled,
-     * autonomous, teleoperated and test.
-     */
-    @Override
-    public void robotPeriodic() {
-        CommandScheduler.getInstance().run();
+  /**
+   * This autonomous runs the autonomous command selected by your {@link OI} class.
+   */
+  @Override
+  public void autonomousInit()
+  {
+    m_OI.setMotorBrake(true);
+    m_autonomousCommand = m_OI.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null)
+    {
+      m_autonomousCommand.schedule();
     }
+  }
 
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic()
+  {
+  }
 
-    @Override
-    public void autonomousInit() {
-        //Example of setting auto: Scheduler.getInstance().add(YOUR AUTO);
+  @Override
+  public void teleopInit()
+  {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null)
+    {
+      m_autonomousCommand.cancel();
+    } else
+    {
+      CommandScheduler.getInstance().cancelAll();
     }
+    m_OI.setDriveMode();
+    m_OI.setMotorBrake(true);
+  }
 
-    /**
-     * This function is called periodically during autonomous.
-     */
-    @Override
-    public void autonomousPeriodic() {
-    }
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic()
+  {
+  }
 
-    /**
-     * This function is called periodically during operator control.
-     */
-    @Override
-    public void teleopPeriodic() {
-    }
+  @Override
+  public void testInit()
+  {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+    m_OI.setDriveMode();
+  }
 
-    /**
-     * This function is called periodically during test mode.
-     */
-    @Override
-    public void testPeriodic() {
-    }
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic()
+  {
+  }
+
+  /**
+   * This function is called once when the robot is first started up.
+   */
+  @Override
+  public void simulationInit()
+  {
+  }
+
+  /**
+   * This function is called periodically whilst in simulation.
+   */
+  @Override
+  public void simulationPeriodic()
+  {
+  }
 }
