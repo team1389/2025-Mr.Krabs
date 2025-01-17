d4969d1917464e2be8e479844742985a87e34c63
Zach Wheeler <92531055+1352351253@users.noreply.github.com>
2025-01-17 14:47:42 -0500

    auto test

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -14,6 +14,7 @@ import java.io.File;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -206,6 +207,6 @@ public class OI {
 
   public Command getAutonomousCommand() {
     //return swerveSubsystem.getAutonomousCommand("scoreClose3");
-    return autoChooser.getSelected();
+        return new PathPlannerAuto("MoveForward3M");
   }
 }
