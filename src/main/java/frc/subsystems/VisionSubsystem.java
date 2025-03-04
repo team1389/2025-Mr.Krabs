a47e455948f97638c065b80c0dfe19fc8805358b
Zach Wheeler <92531055+1352351253@users.noreply.github.com>
2025-03-03 19:33:49 -0500

    help please

--- src/main/java/frc/subsystems/VisionSubsystem.java
+++ src/main/java/frc/subsystems/VisionSubsystem.java
@@ -1,26 +1,28 @@
-// package frc.subsystems;
-// import java.util.Optional;
+package frc.subsystems;
+import java.util.Optional;
 
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import limelight.Limelight;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import limelight.Limelight;
 // import limelight.estimator.PoseEstimate;
 // import limelight.structures.LimelightResults;
 // import limelight.structures.target.pipeline.NeuralClassifier;
 
-// public class VisionSubsystem extends SubsystemBase{
-//     Limelight limelight = new Limelight("limelight");
-//     public double alignTx = 0;
+public class VisionSubsystem extends SubsystemBase{
+    private final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeAndyMark);
+    Limelight limelight = new Limelight("limelight-front");
+    public double alignTx = 0;
 
 
-//     public VisionSubsystem() {
-//         limelight.settingsBuilder().withPipelineIndex(0);
-//     }
+    public VisionSubsystem() {
+    }
 
-//     public double getAlignTX(){
-//         return alignTx;
-//     }
-//     @Override
-//     public void periodic() {
-//         alignTx = limelight.getNTTable().getEntry("tx").getDouble(0);
-//     }
-// }
+    public double getAlignTX(){
+        return alignTx;
+    }
+    @Override
+    public void periodic() {
+        alignTx = limelight.getNTTable().getEntry("tx").getDouble(0);
+    }
+}
