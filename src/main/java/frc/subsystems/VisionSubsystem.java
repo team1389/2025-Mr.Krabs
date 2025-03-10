427871d08ccfb15db9cc260194640c75cbf4eacb
Zach Wheeler <92531055+1352351253@users.noreply.github.com>
2025-03-10 15:49:27 -0400

    added paths and code for auto alignment

--- src/main/java/frc/subsystems/VisionSubsystem.java
+++ src/main/java/frc/subsystems/VisionSubsystem.java
@@ -13,11 +13,16 @@ public class VisionSubsystem extends SubsystemBase{
     private final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeAndyMark);
     Limelight limelight = new Limelight("limelight-front");
     public double alignTx = 0;
+    public double targetID = 0;
     double[] pos = new double[2];
     //pick the default to be whatever is on our wood feild
 
 
     public VisionSubsystem() {
+    }
+
+    public double getTargetID(){
+        return targetID;
     }
 
     public double getAlignTX(){
@@ -39,5 +44,7 @@ public class VisionSubsystem extends SubsystemBase{
         //TODO: test if wpiblue works instead of botpose
         pos = limelight.getNTTable().getEntry("botpose_wpiblue").getDoubleArray(new double[]{0,0,0});
         alignTx = limelight.getNTTable().getEntry("tx").getDouble(0);
+        //TODO: check if it works with double. May have to be integer
+        targetID = limelight.getNTTable().getEntry("tid").getDouble(0);
     }
 }
