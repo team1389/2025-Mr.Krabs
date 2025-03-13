14f0ead586b92a8921e92e42c75a332af0d80e50
Zach Wheeler <92531055+1352351253@users.noreply.github.com>
2025-03-13 18:16:12 -0400

    quick push

--- src/main/java/frc/command/L4.java
+++ src/main/java/frc/command/L4.java
@@ -11,11 +11,13 @@ import frc.subsystems.IntakeSubsystem;
 public class L4 extends SequentialCommandGroup{
     public L4(ElevatorArm elevatorArm){
         addCommands(
+            new SetElevatorAuto(elevatorArm, 117.5555),
             new ParallelCommandGroup(
                 // runs l4 sequence
-                new SetShoulder(elevatorArm, -12.162),
-                new SetElevatorAuto(elevatorArm, 117.5555),
-                new SetWrist(elevatorArm, 265)
+                // new SetElevatorAuto(elevatorArm, 117.5555)
+                new SetWrist(elevatorArm, 265),
+                new SetShoulder(elevatorArm, -12.162)
+
 
             )
             // new TimedOuttakeCoral(intake)
