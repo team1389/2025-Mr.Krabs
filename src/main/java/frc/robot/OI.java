3e87c6cc54254fff472cf0380af2b1a870322308
Ria Gulati <riagulati111@gmail.com>
2025-03-13 16:07:37 -0400

    aight try left bumper

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -211,6 +211,7 @@ public class OI
       // operatorController.x().onTrue(new L2(intake, elevatorArm));
       // operatorController.a().onTrue(new Feeder(intake, elevatorArm));
       operatorController.b().onTrue(new L4(elevatorArm));
+      operatorController.leftBumper().onTrue(new L4Action(elevatorArm, ArmPosition.Starting));
       operatorController.y().onTrue(new L3(intake, elevatorArm));
       // operatorController.button(9).onTrue(new L1(intake, elevatorArm)); // menu
       operatorController.button(14).onTrue(new StartingPos(elevatorArm));
