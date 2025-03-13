cca561b45360477cec2314216c57dfada8481eb4
Zach Wheeler <92531055+1352351253@users.noreply.github.com>
2025-03-13 18:51:33 -0400

    what the sigma

--- src/main/java/frc/command/OuttakeCoral.java
+++ src/main/java/frc/command/OuttakeCoral.java
@@ -1,14 +1,18 @@
 package frc.command;
 
+import edu.wpi.first.wpilibj.Timer;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.subsystems.IntakeSubsystem;
 
 public class OuttakeCoral extends Command {
     public IntakeSubsystem intakeSub;
+    public Timer timer;
 
     public OuttakeCoral(IntakeSubsystem intakeSub) {
         this.intakeSub = intakeSub;
+        timer = new Timer();
         SmartDashboard.putBoolean("IsCoralIn", intakeSub.isCoralIn());
     }
 
@@ -21,5 +25,10 @@ public class OuttakeCoral extends Command {
     @Override
     public void end(boolean interrupted) {
         intakeSub.stopCoral();
+    }
+
+    @Override
+    public boolean isFinished(){
+        return timer.get() > 1.5;
     }
 }
