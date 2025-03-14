cdb6fffcbec2c08afd7fc20770772b0a95ba5481
Ria Gulati <riagulati111@gmail.com>
2025-03-14 14:14:32 -0400

    Anotha one

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -69,6 +69,7 @@ public class OI
   private final Command threePieceThree11Twelve = drivebase.getAutonomousCommand("Three Piece (3, 11, 12)");
   private final Command Top2PieceTwo12 = drivebase.getAutonomousCommand("Top 2 Piece (2, 12)");
   private final Command Top3PieceTwo12Eleven = drivebase.getAutonomousCommand("Top 3 Piece (2, 12, 11)");
+  private final Command Top1Piece2 = drivebase.getAutonomousCommand("Top 1 Piece (2)");
                                                                                 
                                                                               
   /**Leave Starting Area Only
@@ -110,13 +111,14 @@ public class OI
     NamedCommands.registerCommand("Intake", new IntakeCoral(intake));
     NamedCommands.registerCommand("Outtake", new OuttakeCoral(intake));
 
-    m_chooser.setDefaultOption("Simple One Piece Auto", m_simpleOnePieceAuto);
-    m_chooser.addOption("Drive Out Only", m_driveOut);
+    m_chooser.setDefaultOption("Drive Out Only", m_driveOut);
+    m_chooser.addOption("Simple One Piece Auto", m_simpleOnePieceAuto);
     m_chooser.addOption("Two Piece (3, 11)", twoPieceThree11);
     m_chooser.addOption("Top 2 Piece (2, 12)", Top2PieceTwo12);
     m_chooser.addOption("Top 3 Piece (2, 12, 11)", Top3PieceTwo12Eleven);
     m_chooser.addOption("Three Piece (3, 11, 12)", threePieceThree11Twelve);
-    
+    m_chooser.addOption("Top 1 Piece (2)", Top1Piece2);
+
   //post to smart dashboard
     SmartDashboard.putData(m_chooser);
   }
