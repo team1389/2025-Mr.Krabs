72d5497878ab712f57a659ff1e5c5dab87f8e45b
Hacker <100426080+AlphaAlex303@users.noreply.github.com>
2025-10-28 16:31:53 -0400

    Docs - Explained Code Sections
    Added documentation comments for ClimberSubsystem methods and class.

--- src/main/java/frc/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/subsystems/ClimberSubsystem.java
@@ -1,22 +1,40 @@
 package frc.subsystems;
-//lol
+
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
-//landon motor systems are the best
-public class ClimberSubsystem extends SubsystemBase{
+
+/**
+ * The ClimberSubsystem controls the robot’s climbing mechanism.
+ * It manages a single motor used to raise or lower the climber.
+ */
+public class ClimberSubsystem extends SubsystemBase {
+
+    // Motor that powers the climbing system
     private SparkFlex climberMotor;
 
+    /**
+     * Constructor initializes the climber motor.
+     */
     public ClimberSubsystem() {
+        // Create a new SparkFlex motor on the specified port (brushless motor)
         climberMotor = new SparkFlex(RobotMap.MotorPorts.climberMotor, MotorType.kBrushless);
     }
 
-    public void setSpeed(double climberSpeed){
+    /**
+     * Sets the speed of the climber motor.
+     * @param climberSpeed Speed value between -1.0 (down) and 1.0 (up)
+     */
+    public void setSpeed(double climberSpeed) {
         climberMotor.set(climberSpeed);
     }
-    public void stop(){
+
+    /**
+     * Stops the climber motor completely.
+     */
+    public void stop() {
         climberMotor.set(0);
     }
 }
